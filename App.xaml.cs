f9c0d5852e7ad0fb8e9923a2da81d8f10c5ac3e4
margowaluyo <setiawan.margo.waluyo@gmail.com>
2025-11-09 17:53:51 +0700

    fix datetime picker

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace influx2Exporter
 {
@@ -9,6 +11,16 @@ namespace influx2Exporter
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Force UI culture to English (24h format via en-GB)
+            var culture = new CultureInfo("en-GB");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(culture.IetfLanguageTag)));
+            base.OnStartup(e);
+        }
     }
-
 }
